648866da32f02565adf8034091256c07fe88385f
sobutka <108493938+Sobutka@users.noreply.github.com>
2023-11-21 00:19:56 +0000

    Update DentistaController.ts and Dentista.ts

--- src/entities/Dentista.ts
+++ src/entities/Dentista.ts
@@ -1,29 +1,35 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Consulta } from "./Consulta";
 
-@Entity('dentistas')
-export class Dentista{
-    @PrimaryGeneratedColumn()
-    codDent: number
+@Entity("dentistas")
+export class Dentista {
+  @PrimaryGeneratedColumn()
+  codDent: number;
 
-    @Column({type: 'text' })
-    nome: string
+  @Column({ type: "text" })
+  nome: string;
 
-    @Column({type: 'text'})
-    cro: string
+  @Column({ type: "text" })
+  cro: string;
 
-    @Column({type: 'text' })
-    email: string
+  @Column({ type: "text" })
+  email: string;
 
-    @Column({type: 'date' })
-    dataNasc: Date
+  @Column({ type: "date" })
+  dataNasc: Date;
 
-    @Column({type: 'text' })
-    celular: string
-    
-    @Column({type: 'text', nullable: true})
-    celularRecado:string
+  @Column({ type: "text" })
+  celular: string;
 
-    @OneToMany(() => Consulta, (consulta) => consulta.dentista)
-    consulta: Consulta[]
+  @Column({ type: "text", nullable: true })
+  celularRecado: string;
+
+  @Column({ type: "text" })
+  celular: string;
+
+  @Column({ type: "text" })
+  uf: string;
+
+  @OneToMany(() => Consulta, (consulta) => consulta.dentista)
+  consulta: Consulta[];
 }
